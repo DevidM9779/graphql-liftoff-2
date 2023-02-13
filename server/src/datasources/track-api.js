b2f4188253df60d1830d02eff243ec04e2550abe
DevidM9779 <ddavidmilan@hotmail.es>
2023-02-13 12:08:47 -0600

    fixed baseURL to get the API data

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -4,7 +4,7 @@ class TrackAPI extends RESTDataSource {
 
     constructor() {
         super()
-        this.baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/docs/";
+        this.baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
     }
 
     getTracksForHome() {
